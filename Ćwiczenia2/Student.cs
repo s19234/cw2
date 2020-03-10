16a7e34aa3c53e588b10aa567bc1a0333680a3f1
Ryszard <s19234@pjwskt.edu.pl>
2020-03-10 19:45:29 +0100

    Usunięto nie potrzebne atrybuty, dodano metodę Replace by zamienić znak (-) w dacie na (.).

--- Ćwiczenia2/Student.cs
+++ Ćwiczenia2/Student.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Xml.Serialization;
-
-namespace Ćwiczenia2
+﻿namespace Ćwiczenia2
 {
     /// <summary>
     /// Klasa student
     /// </summary>
-    [Serializable]
     public class Student
     {
         /// <summary>
@@ -22,48 +18,38 @@ namespace Ćwiczenia2
             this.LName = studentInfo[1] is null ? "0" : studentInfo[1];
             this.Type = studentInfo[2] is null ? "0" : studentInfo[2];
             this.Type1 = studentInfo[3] is null ? "0" : studentInfo[3];
-            this.Index = studentInfo[4] is null ? "0" : studentInfo[4];
-            this.DateOfBirth = studentInfo[5] is null ? "0" : studentInfo[5];
+            this.Index = studentInfo[4] is null ? "0" : "s" + studentInfo[4];
+            this.DateOfBirth = studentInfo[5] is null ? "0" : studentInfo[5].Replace("-", ".");
             this.Email = studentInfo[6] is null ? "0" : studentInfo[6];
             this.MName = studentInfo[7] is null ? "0" : studentInfo[7];
             this.FName = studentInfo[8] is null ? "0" : studentInfo[8];
         }
 
         // Imie studenta
-        [XmlElement("StudentsName")]
         public string Name { get; set; }
 
         // Nazwisko studenta
-        [XmlElement("StudentsLastName")]
         public string LName { get; set; }
 
-
-        // Typ studiów - informatyka, sztuka nowych mediów
-        [XmlElement("StudentsTypeOfStudies")]
-        public string Type { get; set; }
-
-        // Typ studiów - dzienne, zaoczne
-        [XmlElement("StudentsWhenStudies")]
-        public string Type1 { get; set; }
-
         // Index studenta
-        [XmlElement("StudentsIndex")]
         public string Index { get; set; }
 
         // Data urodzin
-        [XmlElement("StudentsDateOfBirth")]
         public string DateOfBirth { get; set; }
 
         // Email studenta
-        [XmlElement("StudentsEmail")]
         public string Email { get; set; }
 
         // Imie matki studenta
-        [XmlElement("StudentsMothersName")]
         public string MName { get; set; }
 
         // Imie ojca studenta
-        [XmlElement("StudentsFathersName")]
         public string FName { get; set; }
+
+        // Typ studiów - informatyka, sztuka nowych mediów
+        public string Type { get; set; }
+
+        // Typ studiów - dzienne, zaoczne
+        public string Type1 { get; set; }
     }
 }
