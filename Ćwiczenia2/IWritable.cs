8b1f635452a116084676477d4e2cb7c90a1f8829
Ryszard <s19234@pjwskt.edu.pl>
2020-03-16 17:20:40 +0100

    Dodane interfejs wykorzystywany w klasach

---
+++ Ćwiczenia2/IWritable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace Ćwiczenia2
+{
+    interface IWritable
+    {
+        void WriteToXmlFile(string outputFileName);
+        void WriteToJsonFile(string outputFileName);
+    }
+}
